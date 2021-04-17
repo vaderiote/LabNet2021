0de4d3da73c5dc00bf2628b650f2da955c610f1f
vaderiote <rios.vhr@gmail.com>
2021-04-16 23:22:25 -0300

    "proyecto para LabNET"

---
+++ Transporte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1POO
+{
+    public abstract class Transporte
+    {
+        private int pasajeros;
+
+        public int getpasajeros()
+        {
+            return pasajeros;
+        }
+
+
+        public Transporte(int pasajeros)
+        {
+            this.pasajeros = pasajeros;
+        }
+
+        public abstract string Avanzar();
+
+        public abstract string Detenerse();
+    }
+}
