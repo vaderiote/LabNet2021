37ada41262cea71da861c30e892494dae2487907
vaderiote <rios.vhr@gmail.com>
2021-04-17 00:02:45 -0300

    "Proyecto para LabNET"

---
+++ TP1POO/Auto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1POO
+{
+    public class Auto : Transporte
+    {
+
+        public Auto (int pasajeros, string v) : base(pasajeros)
+        {
+
+        }
+
+        public override string Avanzar()
+        {
+            return string.Format("El auto avanza con {0} pasajeros", getpasajeros());
+        }
+
+        public override string Detenerse()
+        {
+            return string.Format("El auto se detiene con {0} pasajeros", getpasajeros());
+        }
+    }
+}
