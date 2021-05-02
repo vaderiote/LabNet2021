9c0b38d2bf10cf1595a4797627623342d3b30fd7
vaderiote <rios.vhr@gmail.com>
2021-05-02 17:55:38 -0300

    TP Entity Framework para LabNET 2021

---
+++ LabNet.TP-EF.Logic/ProductsLogic.cs
@@ -0,0 +1,19 @@
+﻿using LabNet.TP_EF.Data;
+using LabNet.TP_EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNet.TP_EF.Logic
+{
+    public class ProductsLogic : BaseLogic
+    {
+        
+            public List<Products> GetAll()
+            {
+                return context.Products.ToList();
+            }
+        }
+    }
