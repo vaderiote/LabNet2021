bd93a76941c2fc128cdbf288ac583a2474cc3766
vaderiote <rios.vhr@gmail.com>
2021-05-13 13:35:09 -0300

    TP WebAPI con MVC para LabNET 2021

--- LabNet.TP-EF.API/Controllers/ApiController.cs
+++ LabNet.TP-EF.API/Controllers/ApiController.cs
@@ -5,27 +5,32 @@ using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
+using LabNet.TP_EF.API.Models;
 
 namespace LabNet.TP_EF.API.Controllers
 {
     public class ApiController : Controller
     {
-        
+
+
 
         //GET: Api
-        public ActionResult Index()
-        {
-            return View();
-        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        public async System.Threading.Tasks.Task<ActionResult> ApiviewAsync()
-        {
-            string html = "https://swapi.dev/api/people/3/";
-            HttpClient cliente = new HttpClient();
-            var webConstains = await cliente.GetStringAsync(html);
-            var lista = JsonConvert.DeserializeObject<List<IView>>(webConstains);
 
-            return View(lista);
+
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https://digimon-api.vercel.app/api/digimon");
+            List<Root> rootList = JsonConvert.DeserializeObject<List<Root>>(json);
+
+
+            return View(rootList);
 
                  
         }
--- LabNet.TP-EF.API/Models/ApiView.cs
+++ LabNet.TP-EF.API/Models/ApiView.cs
@@ -2,28 +2,12 @@
 
 namespace LabNet.TP_EF.API.Models
 {
-
-    public class SwView
+        public class Root
     {
 
-         public string name { get; set; }
-            public string height { get; set; }
-            public string mass { get; set; }
-            public string hair_color { get; set; }
-            public string skin_color { get; set; }
-            public string eye_color { get; set; }
-            public string birth_year { get; set; }
-            public string gender { get; set; }
-            public string homeworld { get; set; }
-            public string[] films { get; set; }
-            public string[] species { get; set; }
-            public object[] vehicles { get; set; }
-            public object[] starships { get; set; }
-            //public DateTime created { get; set; }
-            //public DateTime edited { get; set; }
-            public string url { get; set; }
-        
-
+        public string name { get; set; }
+        public string img { get; set; }
+        public string level { get; set; }
     }
 
 }
